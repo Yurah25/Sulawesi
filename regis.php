184af95b7f8a763c60fa46c805dd8e9bde5de9ac
Yurah25 <yusufrafiiahmad25@gmail.com>
2025-12-08 18:49:15 +0700

    registrasi sudah 85% untuk htmls css saja nanti dilanjut

--- regis.php
+++ regis.php
@@ -6,12 +6,13 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="assets/css/login.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/regis.css">
     <title>Registrasi-page</title>
 </head>
 <body>
     <div class="deskripsi">
-
+    <h1>Join the Journey Across Sulawesi</h1>
+    <p>Create your account and explore the culture, cuisine, and tourism of Sulawesi in one platform</p>
     </div>
     <form action="" method="post">
         <h1>Login</h1>
