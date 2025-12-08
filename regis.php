a51653488fdf595a3cff4fabc740dd88429fe925
Yurah25 <yusufrafiiahmad25@gmail.com>
2025-12-08 17:54:41 +0700

    untuk sementara membuat login page dengan menyesuaikan ukuran asli nanti untuk selanjutnya registrrasi dan mencoba artikel dan lengkapin gambar home-page

---
+++ regis.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="assets/css/login.css">
+    <title>Registrasi-page</title>
+</head>
+<body>
+    <div class="deskripsi">
+
+    </div>
+    <form action="" method="post">
+        <h1>Login</h1>
+        <label for="email">Email </label>
+        <input type="email" id="email" name="email" placeholder="Masukkan Email">
+        <label for="password">password</label>
+        <input type="password" id="password" name="password" placeholder="Masukkan Password">
+        <a href="">forgot password?</a>
+        <input type="submit" id="submit">
+        <p>dont have an account yet?<a href="regis.php"> Regist here</a></p>
+    </form>
+</body>
+</html>
